5e01656c48a8f5f9e94dc852d354e3ef11a611a6
Lincoln Santiago <incoln.ufgd@gmail.com>
2025-08-12 02:06:05 -0300

    npm config

--- personal-portfolio/server/index.js
+++ personal-portfolio/server/index.js
@@ -152,5 +152,5 @@ ${safe.message}`;
 });
 
 /* --------------------------- Inicialização --------------------------- */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
