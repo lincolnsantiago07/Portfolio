47514919a9be98aaa0f3e40e1d6829ce1ef38456
Lincoln Santiago <incoln.ufgd@gmail.com>
2025-08-11 21:47:11 -0300

    Paths established

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -11,7 +11,7 @@ import { Footer } from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio">
         <NavBar />
         <Banner />
         <Skills />
